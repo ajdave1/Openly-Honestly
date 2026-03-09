c2257e328b03a8f90e5ab8195776b670fb4f50a1
BornToCode <ajdave1@users.noreply.github.com>
2026-03-09 09:14:55 +0100

    new devtoion added

--- dashboard/scripts/dash.js
+++ dashboard/scripts/dash.js
@@ -1,21 +1,31 @@
-const editButtons = document.querySelectorAll("#edit-buttons");
+import { controllers } from "./functions/action.js";
+const { getdevotions } = controllers;
 
+console.log(getdevotions());
+
+const editButtons = document.querySelectorAll("#edit-buttons");
 const editContainer = document.querySelector(".pop-up-container");
 
 editButtons.forEach((button) => {
   button.addEventListener("click", () => {
     let page = button.dataset.edit;
 
-    let week_topics = document.querySelector(".weekly-topics");
+    let week_topics = document.querySelector(".update-weekly-topics");
 
-    let daily_devotions = document.querySelector(".daily-devotions");
+    let daily_devotions = document.querySelector(".update-daily-devotions");
 
     editContainer.style = "display:flex;";
 
     if (page == "week") {
       daily_devotions.classList.add("hide-item");
+      if (week_topics.classList.contains("hide-item")) {
+        week_topics.classList.remove("hide-item");
+      }
     } else if (page == "devotions") {
       week_topics.classList.add("hide-item");
+      if (daily_devotions.classList.contains("hide-item")) {
+        daily_devotions.classList.remove("hide-item");
+      }
     }
   });
 });
--- dashboard/scripts/functions/action.js
+++ dashboard/scripts/functions/action.js
@@ -1,7 +1,18 @@
-const actionFunctions = {
-  showEdit_container: (button) => {
-    button.addEventListener("click", () => {
-      console.log(button);
+export const controllers = {
+  getdevotions: async () => {
+    const api_url = "https://localhost/openlyandhonestly/devotion_api.php";
+
+    const allDevotions = await fetch(api_url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        action: "read_all",
+      }),
     });
+
+    const data = await allDevotions.json();
+    return data;
   },
 };
