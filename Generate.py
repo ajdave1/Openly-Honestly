a69589678eb1905944fc8ba5a2488e69fd3072c4
BornToCode <ajdave1@users.noreply.github.com>
2026-06-13 02:11:51 +0100

    DAY 6

--- Generate.py
+++ Generate.py
@@ -1,20 +1,3 @@
-import json
-from http.client import responses
-
-from openai import OpenAI
-apiKey = "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"
-
-intellegence = OpenAI(api_key =  apiKey)
-
-responses = intellegence.responses.create(model="gpt-3.5-turbo",input="hello")
-
-
-print(responses)
-
-
-with open("devotions.json", encoding='utf-8') as devotionsJSON:
-    data = json.load(devotionsJSON)
-
 
 def generateDevotions():
     ...
